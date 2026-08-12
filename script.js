dd9ea83b80b13e94931abb38ac2e906bd3a0578e
Subham Verma <shubhamverma17092005@gmail.com>
2026-08-12 11:35:48 +0530

    feat: select button and add click event listener for h1, changes color & text

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+//Title
 const header = document.querySelector('#title');   // by id
 console.log(header.textContent);
 header.textContent = 'Blur';
 header.style.color = 'blue';
 
-const des = document.querySelector('.description');   // by class
-des.textContent = 'Learning DOM manipulation';
+//Description
+const para = document.querySelector('.description');   // by class
+para.textContent = 'Learning DOM manipulation';
+
+//Button
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    header.textContent = 'Button Clicked!';
+    header.style.color = 'red';
+});
 
