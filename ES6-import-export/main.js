20a1f1c20babd8118103976d26597a8f9a7a2096
Subham Verma <shubhamverma17092005@gmail.com>
2026-08-29 10:54:26 +0530

    feat: demonstrate ES6 modules with named exports/imports across math.js and main.js

---
+++ ES6-import-export/main.js
@@ -0,0 +1,11 @@
+// main.js
+import {add, subtract, multiply} from './math.js';
+
+console.log(add(1000, 500));
+console.log(subtract(1000, 500));
+console.log(multiply(1000, 500));
+
+//Output
+// 1500
+// 500
+// 500000
